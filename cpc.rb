752e81d1744a4a709ab9672590b617c0352924c2
Corey Farwell <coreyf@rwell.org>
2013-02-08 23:43:23 -0800

    the singleton isn't really necessary, just experimenting

---
+++ cpc.rb
@@ -0,0 +1,41 @@
+require 'singleton'
+
+
+class CPC
+   include Singleton
+
+   @user = ENV['USERNAME']
+   @args = ARGV[1..-1]
+
+   def submissions
+      raise NotImplementedError
+   end
+   
+   def scoreboard
+      raise NotImplementedError
+   end
+
+   def grade
+      raise NotImplementedError
+   end
+
+   def contests
+      raise NotImplementedError
+   end
+
+   def register
+      raise NotImplementedError
+   end
+
+   def problems
+      raise NotImplementedError
+   end
+
+   def help
+      raise NotImplementedError
+   end
+end
+
+
+command = ARGV[0].to_s.strip.to_sym
+CPC.instance.method(command).call
