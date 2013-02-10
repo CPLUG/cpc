77e5f6ed27068df133dea50f3ea48b4a5d70afa0
Peter Faiman <pfaiman@calpoly.edu>
2013-02-09 20:19:54 -0800

    some serious command layout

--- cpc.rb
+++ cpc.rb
@@ -1,46 +1,116 @@
+#!/usr/bin/env ruby
+#change to #!/home/pfaiman/ruby/bin/ruby to prevent tampering with runtime
+
+if __FILE__ != $0
+    puts 'Do not include or require this file.'
+    exit!
+end
+
+$commands_summary = <<eos
+The following commands are available:
+help           View help text on a command
+submissions    View your submissions
+scoreboard     View the scoreboard
+grade          Check your grade
+contests       List running contests
+register       Register for the contest
+problems       List problems
+eos
+
+# fix double newlines in help display
+$commands_summary.rstrip!
+
+$commands = {
+    :submissions => <<eos,
+Usage: cpc submissions
+
+Description
+eos
+    :scoreboard => <<eos,
+Usage: cpc scoreboard
+
+Description
+eos
+    :grade => <<eos,
+Usage: cpc grade
+
+Description
+eos
+    :contests => <<eos,
+Usage: cpc contests
+
+Description
+eos
+    :register => <<eos,
+Usage: cpc register
+
+Description
+eos
+    :problems => <<eos,
+Usage: cpc problems
+
+Description
+eos
+    :help => <<eos,
+Usage: cpc help <command>
+
+#{$commands_summary}
+eos
+}
+
 def submissions(user, args)
-   raise NotImplementedError
+    false
 end
 
 def scoreboard(user, args)
-   raise NotImplementedError
+    false
 end
 
 def grade(user, args)
-   raise NotImplementedError
+    false
 end
 
 def contests(user, args)
-   raise NotImplementedError
+    false
 end
 
 def register(user, args)
-   raise NotImplementedError
+    false
 end
 
 def problems(user, args)
-   raise NotImplementedError
+    false
 end
 
-def help(user, args)
-   raise NotImplementedError
+def help(user, cmd)
+    cmd = cmd.strip.downcase.to_sym unless cmd.kind_of?(Symbol)
+    if $commands.include? cmd
+        puts $commands[cmd]
+        true
+    else
+        puts <<eos
+No such command.
+
+#{$commands_summary}
+eos
+        false
+    end
 end
 
-command = ARGV[0].to_s.strip.downcase
-commands = {
-   "submissions" => Object.method(:submissions),
-   "scoreboard" => Object.method(:scoreboard),
-   "grade" => Object.method(:grade),
-   "contests" => Object.method(:contests),
-   "register" => Object.method(:register),
-   "problems" => Object.method(:problems),
-   "help" => Object.method(:help)
-}
+if ARGV[0] == nil
+    puts <<eos
+Usage: cpc <command>
 
-if command.length == 0
-   puts 'usage: cpc <command>'
-elsif not commands.include? command
-   puts "'#{command}' is not a valid command"
+#{$commands_summary}
+eos
 else
-   commands[command].call(ENV['USERNAME'], ARGV[1..-1])
+    cmd = ARGV[0].strip.downcase.to_sym
+    ARGV[0] = `/usr/bin/whoami`.strip
+    if $commands.include? cmd
+        cmd_f = method cmd
+        if ARGV.length == cmd_f.arity || cmd_f.arity == -1
+            exit(cmd_f.call *ARGV)
+        end
+    end
+    exit(help *[ARGV[0], cmd])
 end
